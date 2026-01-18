bacea0327b1128748a4f9aae16bdd5b1fc6844df
laurabni <bornilaura295@gmail.com>
2026-01-18 22:39:33 +0100

    Review

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -20,7 +20,7 @@ const renderText = (text, className, baseWeight = 400) => {
 };
 
 const setupTextHover = (container, type) => {
-    if (!container) return;
+    if (!container) return () => {};
 
     const letters = container.querySelectorAll('span');
     const { min, max, default: base } = FONT_WEIGHTS[type];
@@ -29,7 +29,7 @@ const setupTextHover = (container, type) => {
         return gsap.to(letter, {
             duration,
             ease: "power2.out",
-            fontVariationSettings: `'wght' ${weight}'`,
+            fontVariationSettings: `'wght' ${weight}`,
         })
     };
 
